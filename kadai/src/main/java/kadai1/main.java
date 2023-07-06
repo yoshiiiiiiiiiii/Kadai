868d9768e40d94d10172df3f642d588815b45e8e
unknown <dokunari@i.softbank.jp>
2023-07-06 15:37:23 +0900

    課題1

---
+++ kadai/src/main/java/kadai1/main.java
@@ -0,0 +1,39 @@
+package kadai1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// 設問１
+		
+		// 変数
+		int test1 = 100;
+		int test2 = 66;
+		int test3 = 33;
+		int testAve1 = (test1 + test2 + test3) / 3;
+		
+		System.out.println("平均点(int型)は" + testAve1 + "点です。");
+		
+		// 設問２
+		
+		// 変数
+		float test4 = 100;
+		float test5 = 66;
+		float test6 = 33;
+		float testAve2 = (test4 + test5 + test6) / 3;
+		
+		System.out.println("平均点（float型）は" + testAve2 + "点です。");
+		
+		// 設問３
+		
+		// 変数
+		double test7 = 100;
+		double test8 = 66;
+		double test9 = 33;
+		double testAve3 = (test7 + test8 + test9) / 3;
+		
+		System.out.println("平均点（double型）は" + testAve3 + "点です。");
+
+	}
+
+}
+
