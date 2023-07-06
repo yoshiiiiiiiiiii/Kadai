eb8e77c9013e775c790190a4df1b7a5bbc17fa89
unknown <dokunari@i.softbank.jp>
2023-07-06 18:30:49 +0900

    課題２

---
+++ kadai/src/main/java/kadai2/main.java
@@ -0,0 +1,19 @@
+package kadai2;
+
+/**
+ * 課題２クラス
+ * 文字列の長さの表示ができる
+ */
+public class main {
+
+	public static void main(String[] args) {
+		// 設問２
+		
+		// 文字列の長さ
+		String corpName = "AlphaSystems";
+		int len = corpName.length();
+		System.out.println(corpName + "の文字の長さは" + len +"文字です。");
+
+	}
+
+}
